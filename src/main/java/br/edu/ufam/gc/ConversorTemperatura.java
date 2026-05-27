d569822183d6629b6b2b251a20c63d1aa95a227d
Uriel <urielfarias21@gmail.com>
2026-05-27 10:17:52 -0400

    projeto para o maven

---
+++ src/main/java/br/edu/ufam/gc/ConversorTemperatura.java
@@ -0,0 +1,20 @@
+package br.edu.ufam.gc;
+
+public class ConversorTemperatura {
+
+    public double celsiusParaFahrenheit(double celsius) {
+        return celsius * 9.0 / 5.0 + 32;
+    }
+
+    public double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0 / 9.0;
+    }
+
+    public double celsiusParaKelvin(double celsius) {
+        return celsius + 273.15;
+    }
+
+    public double kelvinParaCelsius(double kelvin) {
+        return kelvin - 273.15;
+    }
+}
---
+++ src/test/java/br/edu/ufam/gc/ConversorTemperaturaTest.java
@@ -0,0 +1,45 @@
+package br.edu.ufam.gc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConversorTemperaturaTest {
+
+    private ConversorTemperatura conversor;
+
+    @BeforeEach
+    void setUp() {
+        conversor = new ConversorTemperatura();
+    }
+
+    @Test
+    void celsiusParaFahrenheit_aguaFervendo() {
+        assertEquals(212.0, conversor.celsiusParaFahrenheit(100.0), 0.01);
+    }
+
+    @Test
+    void celsiusParaFahrenheit_aguaCongelando() {
+        assertEquals(32.0, conversor.celsiusParaFahrenheit(0.0), 0.01);
+    }
+
+    @Test
+    void fahrenheitParaCelsius_aguaFervendo() {
+        assertEquals(100.0, conversor.fahrenheitParaCelsius(212.0), 0.01);
+    }
+
+    @Test
+    void fahrenheitParaCelsius_aguaCongelando() {
+        assertEquals(0.0, conversor.fahrenheitParaCelsius(32.0), 0.01);
+    }
+
+    @Test
+    void celsiusParaKelvin_zeroAbsoluto() {
+        assertEquals(0.0, conversor.celsiusParaKelvin(-273.15), 0.01);
+    }
+
+    @Test
+    void kelvinParaCelsius_zeroAbsoluto() {
+        assertEquals(-273.15, conversor.kelvinParaCelsius(0.0), 0.01);
+    }
+}
